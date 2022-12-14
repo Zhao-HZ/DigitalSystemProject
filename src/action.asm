19cfbc45076169a9c2a5dcc480c56b3b7fe3b559
ZHZ <18050086866@qq.com>
2022-12-14 16:19:17 +0800

    Add 3 Scenes, Scrolling and change

---
+++ src/action.asm
@@ -0,0 +1,98 @@
+SECTION "action", ROM0
+
+waitsometime:
+    ld c, 224
+    ld b, 100
+    ld d, 224
+repeat1:
+repeat2:
+repeat3:
+    WaitVBlank__:
+    ld a, [rLY]
+    cp 144
+    jp nz, WaitVBlank__
+    dec c
+    jp nz, repeat3
+    jp nz, repeat2
+    jp nz, repeat1
+    ret
+
+; arguments
+; hl --- memory addr that start to clear
+;        the first element of one line
+; c  --- row 
+; b  --- column
+clear_screen:
+clear_all:
+	push hl
+clear:
+	ld a, 0
+	ld [hli], a
+	dec c
+	jp nz, clear
+
+	pop hl
+	ld de, $0020
+	add hl, de
+	dec b
+	jp nz, clear_all
+	ret	
+
+close_LCD:
+    ld a, [rLCDC]
+    res 7, a
+    ld [rLCDC], a
+    ret
+
+open_LCD:
+    ld a, [rLCDC]
+    set 7, a
+    ld [rLCDC], a
+    ret
+
+display_scene1:
+    ld de, Scene1
+	ld hl, $9800
+	ld bc, Scene1End - Scene1
+CopyTilemap1:
+	ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTilemap1
+    ret
+
+display_scene2:
+    call close_LCD
+    ld de, Scene2
+	ld hl, $9800
+	ld bc, Scene2End - Scene2
+CopyTilemap2:
+	ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTilemap2
+    call open_LCD
+    ret
+
+display_scene3:
+    call close_LCD 
+    ld de, Scene3
+	ld hl, $9800
+	ld bc, Scene3End - Scene3
+
+    CopyTilemap3:
+	ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, CopyTilemap3
+    call open_LCD
+    ret
--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,6 @@
 INCLUDE "./src/hardware.inc"
 INCLUDE "./src/data.inc"
+INCLUDE "./src/action.asm"
 
 SECTION "Header", ROM0[$100]
 
@@ -19,11 +20,12 @@ WaitVBlank:
     jp c, WaitVBlank
 
     ; Turn the LCD off
-    ld a, 1
+    ld a, 0
     ld [rLCDC], a
+    ; call close_LCD
 
 	; Copy the tile data
-	ld de, Tiles
+	ld de, Tiles 
 	ld hl, $9000
 	ld bc, TilesEnd - Tiles
 CopyTiles:
@@ -35,27 +37,52 @@ CopyTiles:
 	or a, c
 	jp nz, CopyTiles
 	
-	ld de, Scene3
-	ld hl, $9800
-	ld bc, Scene3End - Scene3
-CopyTilemap:
-	ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyTilemap
+    call display_scene1
 
     ; Turn the LCD on
     ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON
     ; ld a, 1
     ld [rLCDC], a
+    ; call open_LCD
 
-    ; During the first (blank) frame, initialize display reg  isters
+    ; Dring the first (blank) frame, initialize display reg  isters
     ld a, %11100100
     ld [rBGP], a
-    ld [rOBP1], a
+	
+	call waitsometime
+	call waitsometime
+	call waitsometime
+	call waitsometime
+
+	ld c, $73
+    ld a, [rSCY]
+    ld b, a
+repeat_scroll:
+	wait_:
+	ld a, [rLY]
+	cp 144
+	jp nz, wait_
+	ld a, [rSCY]   
+	add a, 1
+	ld [rSCY], a
+	dec c
+	jp nz, repeat_scroll
+	
+	call waitsometime
+    call waitsometime
+    call waitsometime
+    call waitsometime
+    call waitsometime
+
+    ld hl, $9820
+    ld c, $13
+    ld b, $13 
+    call clear_screen
+
+    ld a, 0
+    ld [rSCY], a
+
+    call display_scene3    
 
 Done:
 	jp Done	
